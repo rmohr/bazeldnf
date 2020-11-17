4b0ae19c2972f444bf9062c1ca75a933abd945e4
Roman Mohr <rmohr@redhat.com>
2020-11-17 17:45:25 +0100

    Initial write support for rpmtree rules
    Signed-off-by: Roman Mohr <rmohr@redhat.com>

--- pkg/bazel/bazel.go
+++ pkg/bazel/bazel.go
@@ -27,6 +27,26 @@ func LoadWorkspace(path string) (*build.File, error) {
 	return workspace, nil
 }
 
+func LoadBuild(path string) (*build.File, error) {
+	buildfileData, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse WORSPACE orig: %v", err)
+	}
+	buildfile, err := build.ParseBuild(path, buildfileData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse WORSPACE orig: %v", err)
+	}
+	return buildfile, nil
+}
+
+func WriteBuild(dryRun bool, buildfile *build.File, path string) error {
+	if dryRun {
+		fmt.Println(build.FormatString(buildfile))
+		return nil
+	}
+	return ioutil.WriteFile(path, build.Format(buildfile), 0666)
+}
+
 func WriteWorkspace(dryRun bool, workspace *build.File, path string) error {
 	if dryRun {
 		fmt.Println(build.FormatString(workspace))
@@ -44,9 +64,9 @@ func AddRPMS(workspace *build.File, pkgs []*api.Package) {
 	}
 
 	for _, pkg := range pkgs {
-		call := &build.CallExpr{X: &build.Ident{Name: "rpm"}}
 		rule := rpms[pkg.String()]
 		if rule == nil {
+			call := &build.CallExpr{X: &build.Ident{Name: "rpm"}}
 			rule = &rpmRule{&build.Rule{call, ""}}
 			rpms[pkg.String()] = rule
 		}
@@ -70,6 +90,47 @@ func AddRPMS(workspace *build.File, pkgs []*api.Package) {
 	workspace.DelRules("rpm", "")
 	for _, rule := range rules {
 		workspace.Stmt = edit.InsertAtEnd(workspace.Stmt, rule.Call)
+	}
+}
+
+func AddTree(name string, buildfile *build.File, pkgs []*api.Package, files []string) {
+	rpmtrees := map[string]*rpmTree{}
+
+	for _, rule := range buildfile.Rules("rpmtree") {
+		rpmtrees[rule.Name()] = &rpmTree{rule}
+	}
+
+	rpms := []string{}
+	for _, pkg := range pkgs {
+		rpms = append(rpms, "@"+pkg.Name+"//rpm")
+	}
+
+	sort.SliceStable(files, func(i, j int) bool {
+		return files[i] < files[j]
+	})
+
+	rule := rpmtrees[name]
+	if rule == nil {
+		call := &build.CallExpr{X: &build.Ident{Name: "rpmtree"}}
+		rule = &rpmTree{&build.Rule{call, ""}}
+		rpmtrees[name] = rule
+	}
+	rule.SetName(name)
+	rule.SetRPMs(rpms)
+	rule.SetFiles(files)
+
+	rules := []*rpmTree{}
+	for _, rule := range rpmtrees {
+		rules = append(rules, rule)
+	}
+
+	sort.SliceStable(rules, func(i, j int) bool {
+		return rules[i].Name() < rules[j].Name()
+	})
+
+
+	for _, rule := range rules {
+		buildfile.Stmt = edit.InsertAtEnd(buildfile.Stmt, rule.Call)
 	}
 }
 
@@ -104,4 +165,41 @@ func (r *rpmRule) SetName(name string) {
 
 func (r *rpmRule) SetSHA256(sha256 string) {
 	r.Rule.SetAttr("sha256", &build.StringExpr{Value: sha256})
+}
+
+type rpmTree struct {
+	*build.Rule
+}
+
+func (r *rpmTree) SetName(name string) {
+	r.Rule.SetAttr("name", &build.StringExpr{Value: name})
+}
+
+func (r *rpmTree) RPMs() []string {
+	if rpmAttrs := r.Rule.Attr("rpms"); rpmAttrs != nil {
+		if len(rpmAttrs.(*build.ListExpr).List) > 0 {
+			rpms := []string{}
+			for _, expr := range rpmAttrs.(*build.ListExpr).List {
+				rpms = append(rpms, expr.(*build.StringExpr).Value)
+			}
+			return rpms
+		}
+	}
+	return nil
+}
+
+func (r *rpmTree) SetRPMs(rpms []string) {
+	rpmsAttr := []build.Expr{}
+	for _, rpm := range rpms {
+		rpmsAttr = append(rpmsAttr, &build.StringExpr{Value: rpm})
+	}
+	r.Rule.SetAttr("rpms", &build.ListExpr{List: rpmsAttr})
+}
+
+func (r *rpmTree) SetFiles(files []string) {
+	filesAttr := []build.Expr{}
+	for _, file := range files {
+		filesAttr = append(filesAttr, &build.StringExpr{Value: file})
+	}
+	r.Rule.SetAttr("files", &build.ListExpr{List: filesAttr})
 }
--- pkg/bazel/bazel_test.go
+++ pkg/bazel/bazel_test.go
@@ -1,6 +1,7 @@
 package bazel
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -10,7 +11,7 @@ import (
 	"github.com/rmohr/bazeldnf/pkg/api/bazeldnf"
 )
 
-func TestPruneRPMs(t *testing.T) {
+func TestWorkspaceWithRPMs(t *testing.T) {
 	tests := []struct {
 		name     string
 		orig     string
@@ -42,6 +43,46 @@ func TestPruneRPMs(t *testing.T) {
 
 			current, err := ioutil.ReadFile(tmpFile.Name())
 			g.Expect(err).ToNot(HaveOccurred())
+			expected, err := ioutil.ReadFile(tt.expected)
+			g.Expect(err).ToNot(HaveOccurred())
+			g.Expect(string(current)).To(Equal(string(expected)))
+		})
+	}
+}
+
+func TestBuildfileWithRPMs(t *testing.T) {
+	tests := []struct {
+		name     string
+		orig     string
+		expected string
+		pkgs     []*api.Package
+	}{
+		{
+			name:     "should replace rpm entries",
+			orig:     "testdata/BUILD.bazel.test",
+			expected: "testdata/BUILD.bazel.result",
+			pkgs: []*api.Package{
+				newPkg("a", "1.2.3", repo("a", []string{"a", "b", "c"})),
+				newPkg("a", "2.3.4", repo("b", []string{"e", "f", "g"})),
+				newPkg("b", "2.3.4", repo("a", []string{"a", "b", "c"})),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
+			tmpFile, err := ioutil.TempFile(os.TempDir(), "BUILD")
+			g.Expect(err).ToNot(HaveOccurred())
+			defer os.Remove(tmpFile.Name())
+			file, err := LoadBuild(tt.orig)
+			g.Expect(err).ToNot(HaveOccurred())
+			AddTree("mytree", file, tt.pkgs, []string{"a", "b"})
+			err = WriteBuild(false, file, tmpFile.Name())
+			g.Expect(err).ToNot(HaveOccurred())
+
+			current, err := ioutil.ReadFile(tmpFile.Name())
+			g.Expect(err).ToNot(HaveOccurred())
+			fmt.Println(string(current))
 			expected, err := ioutil.ReadFile(tt.expected)
 			g.Expect(err).ToNot(HaveOccurred())
 			g.Expect(string(current)).To(Equal(string(expected)))
