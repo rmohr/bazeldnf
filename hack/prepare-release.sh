797543f9ea97d2559aaa89128290866ed5ae05da
Manuel Naranjo <manuel.naranjo@booking.com>
2024-07-14 11:07:02 +0200

    prebuilts: reorganize how we consume prebuilts
    Prebuilts are now handled differently and require a proper release
    process, in order to allow forks to also release we need to consume
    GITHUB_REPOSITORY while rendering the repo files.

--- hack/prepare-release.sh
+++ hack/prepare-release.sh
@@ -9,6 +9,8 @@ BASE_DIR="$(
 	pwd
 )"
 
+REPO_URL="${GITHUB_REPOSITORY:-rmohr/bazeldnf}"
+
 rm -rf dist
 mkdir -p dist
 
@@ -25,13 +27,8 @@ function write_arch() {
 	arch=$2
 
 	DIGEST=$(sha256sum dist/bazeldnf-${VERSION}-${os}-${arch} | cut -d " " -f 1)
-	cat <<EOT >>bazeldnf/deps.bzl
-    http_file(
-        name = "bazeldnf-${os}-${arch}",
-        executable = True,
-        sha256 = "${DIGEST}",
-        urls = ["https://github.com/rmohr/bazeldnf/releases/download/${VERSION}/bazeldnf-${VERSION}-${os}-${arch}"],
-    )
+	cat <<EOT >>bazeldnf/private/prebuilts.bzl
+	"${os}-${arch}": "${DIGEST}",
 EOT
 
 }
@@ -44,42 +41,14 @@ build_arch linux ppc64
 build_arch linux ppc64le
 build_arch linux s390x
 
-cat <<EOT >bazeldnf/deps.bzl
-"""bazeldnf public dependency for WORKSPACE"""
+cat <<EOT >bazeldnf/private/prebuilts.bzl
+"""Expose prebuilt binaries for bazeldnf."""
 
-load(
-    "@bazel_tools//tools/build_defs/repo:http.bzl",
-    "http_archive",
-    "http_file",
-)
-load("@bazel_tools//tools/build_defs/repo:utils.bzl", "maybe")
-load(
-    "@bazeldnf//internal:rpm.bzl",
-    _rpm = "rpm",
-)
+VERSION = "${VERSION}"
 
-rpm = _rpm
+REPO_URL = "${REPO_URL}"
 
-def bazeldnf_dependencies():
-    """bazeldnf dependencies when consuming the repo externally"""
-    maybe(
-        http_archive,
-        name = "bazel_skylib",
-        sha256 = "f24ab666394232f834f74d19e2ff142b0af17466ea0c69a3f4c276ee75f6efce",
-        urls = [
-            "https://mirror.bazel.build/github.com/bazelbuild/bazel-skylib/releases/download/1.4.0/bazel-skylib-1.4.0.tar.gz",
-            "https://github.com/bazelbuild/bazel-skylib/releases/download/1.4.0/bazel-skylib-1.4.0.tar.gz",
-        ],
-    )
-    maybe(
-        http_archive,
-        name = "platforms",
-        urls = [
-            "https://mirror.bazel.build/github.com/bazelbuild/platforms/releases/download/0.0.10/platforms-0.0.10.tar.gz",
-            "https://github.com/bazelbuild/platforms/releases/download/0.0.10/platforms-0.0.10.tar.gz",
-        ],
-        sha256 = "218efe8ee736d26a3572663b374a253c012b716d8af0c07e842e82f238a0a7ee",
-    )
+PREBUILTS = {
 EOT
 
 write_arch linux amd64
@@ -90,9 +59,12 @@ write_arch linux ppc64
 write_arch linux ppc64le
 write_arch linux s390x
 
+echo >>bazeldnf/private/prebuilts.bzl "}"
+
 git commit -a -m "Bump prebuilt binary references for ${VERSION}"
 
 git tag ${VERSION}
+
 
 git archive --format tar.gz HEAD >./dist/bazeldnf-${VERSION}.tar.gz
 
@@ -106,7 +78,7 @@ http_archive(
     name = "bazeldnf",
     sha256 = "${DIGEST}",
     urls = [
-        "https://github.com/rmohr/bazeldnf/releases/download/${VERSION}/bazeldnf-${VERSION}.tar.gz",
+        "https://github.com/${REPO_URL}/releases/download/${VERSION}/bazeldnf-${VERSION}.tar.gz",
     ],
 )
 
