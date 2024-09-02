2c78a4094bfdd03b64d23ef17a919a0e939cdc8d
Manuel Naranjo <manuel.naranjo@booking.com>
2024-09-02 13:43:21 +0200

    go: allow to configure logrus level
    Now the logrus level can be changed through the  environment variable
    LOG_LEVEL

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -16,6 +17,15 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	if logLevel, hasIt := os.LookupEnv("LOG_LEVEL") ; hasIt {
+		level, err := logrus.ParseLevel(logLevel)
+		if err != nil {
+			fmt.Println("Unable to parse log level from environment variable LOG_LEVEL")
+			os.Exit(1)
+		}
+		logrus.SetLevel(level)
+	}
+
 	rootCmd.AddCommand(NewXATTRCmd())
 	rootCmd.AddCommand(NewSandboxCmd())
 	rootCmd.AddCommand(NewFetchCmd())
