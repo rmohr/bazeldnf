b3db1359765d8262fb4aead19903213162c66cf5
Michael Kelly <mkelly@arista.com>
2024-11-14 16:46:11 -0800

    Factor our rpmtree handlers

--- cmd/rpmtree.go
+++ cmd/rpmtree.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bazelbuild/buildtools/build"
 	"github.com/rmohr/bazeldnf/cmd/template"
+	"github.com/rmohr/bazeldnf/pkg/api"
 	"github.com/rmohr/bazeldnf/pkg/bazel"
 	"github.com/rmohr/bazeldnf/pkg/reducer"
 	"github.com/rmohr/bazeldnf/pkg/repo"
@@ -27,6 +28,77 @@ type rpmtreeOpts struct {
 }
 
 var rpmtreeopts = rpmtreeOpts{}
+
+type Handler interface {
+	AddRPMs(pkgs []*api.Package, arch string) error
+	PruneRPMs(buildfile *build.File)
+	Write() error
+}
+
+type MacroHandler struct {
+	bzl, defName string
+	bzlfile      *build.File
+}
+
+func NewMacroHandler(toMacro string) (Handler, error) {
+	bzl, defName, err := bazel.ParseMacro(rpmtreeopts.toMacro)
+
+	if err != nil {
+		return nil, err
+	}
+
+	bzlfile, err := bazel.LoadBzl(bzl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &MacroHandler{
+		bzl:     bzl,
+		bzlfile: bzlfile,
+		defName: defName,
+	}, nil
+}
+
+func (h *MacroHandler) AddRPMs(pkgs []*api.Package, arch string) error {
+	return bazel.AddBzlfileRPMs(h.bzlfile, h.defName, pkgs, arch)
+}
+
+func (h *MacroHandler) PruneRPMs(buildfile *build.File) {
+	bazel.PruneBzlfileRPMs(buildfile, h.bzlfile, h.defName)
+}
+
+func (h *MacroHandler) Write() error {
+	return bazel.WriteBzl(false, h.bzlfile, h.bzl)
+}
+
+type WorkspaceHandler struct {
+	workspace     string
+	workspacefile *build.File
+}
+
+func NewWorkspaceHandler(workspace string) (Handler, error) {
+	workspacefile, err := bazel.LoadWorkspace(workspace)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WorkspaceHandler{
+		workspace: workspace,
+		workspacefile: workspacefile,
+	}, nil
+}
+
+func (h *WorkspaceHandler) AddRPMs(pkgs []*api.Package, arch string) error {
+	return bazel.AddWorkspaceRPMs(h.workspacefile, pkgs, arch)
+}
+
+func (h *WorkspaceHandler) PruneRPMs(buildfile *build.File) {
+	bazel.PruneWorkspaceRPMs(buildfile, h.workspacefile)
+}
+
+func (h *WorkspaceHandler) Write() error {
+	return bazel.WriteWorkspace(false, h.workspacefile, h.workspace)
+}
 
 func NewRpmTreeCmd() *cobra.Command {
 
@@ -68,54 +140,37 @@ func NewRpmTreeCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			workspace, err := bazel.LoadWorkspace(rpmtreeopts.workspace)
+
+			var handler Handler
+			if writeToMacro {
+				handler, err = NewMacroHandler(rpmtreeopts.toMacro)
+			} else {
+				handler, err = NewWorkspaceHandler(rpmtreeopts.workspace)
+			}
+			
 			if err != nil {
 				return err
 			}
-			var bzlfile *build.File
-			var bzl, defName string
-			if writeToMacro {
-				bzl, defName, err = bazel.ParseMacro(rpmtreeopts.toMacro)
-				if err != nil {
-					return err
-				}
-				bzlfile, err = bazel.LoadBzl(bzl)
-				if err != nil {
-					return err
-				}
-			}
+			
 			build, err := bazel.LoadBuild(rpmtreeopts.buildfile)
 			if err != nil {
 				return err
 			}
-			if writeToMacro {
-				err = bazel.AddBzlfileRPMs(bzlfile, defName, install, rpmtreeopts.arch)
-				if err != nil {
-					return err
-				}
-			} else {
-				err = bazel.AddWorkspaceRPMs(workspace, install, rpmtreeopts.arch)
-				if err != nil {
-					return err
-				}
-			}
-			bazel.AddTree(rpmtreeopts.name, build, install, rpmtreeopts.arch, rpmtreeopts.public)
-			if writeToMacro {
-				bazel.PruneBzlfileRPMs(build, bzlfile, defName)
-			} else {
-				bazel.PruneWorkspaceRPMs(build, workspace)
-			}
-			logrus.Info("Writing bazel files.")
-			err = bazel.WriteWorkspace(false, workspace, rpmtreeopts.workspace)
+
+			err = handler.AddRPMs(install, rpmtreeopts.arch)
 			if err != nil {
 				return err
 			}
-			if writeToMacro {
-				err = bazel.WriteBzl(false, bzlfile, bzl)
-				if err != nil {
-					return err
-				}
+
+			bazel.AddTree(rpmtreeopts.name, build, install, rpmtreeopts.arch, rpmtreeopts.public)
+			
+			handler.PruneRPMs(build)
+			logrus.Info("Writing bazel files.")
+			err = handler.Write()
+			if err != nil {
+				return err
 			}
+
 			err = bazel.WriteBuild(false, build, rpmtreeopts.buildfile)
 			if err != nil {
 				return err
