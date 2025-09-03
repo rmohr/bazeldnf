18094429a0a2090098eac2f31253d286d1f10785
Bennett Zhu <bzhu@confluent.io>
2025-09-03 14:46:27 -0700

    order

--- pkg/xattr/testdata/regenerate.sh
+++ pkg/xattr/testdata/regenerate.sh
@@ -6,15 +6,15 @@ trap 'rm -rf "${TMPDIR}"' EXIT
 touch ${TMPDIR}/selinux
 touch ${TMPDIR}/cap_net_bind_service
 touch ${TMPDIR}/cap_chown
-touch ${TMPDIR}/cap_sys_ptrace
 touch ${TMPDIR}/cap_net_admin
 touch ${TMPDIR}/cap_net_raw
+touch ${TMPDIR}/cap_sys_ptrace
 touch ${TMPDIR}/cap_all
 sudo chcon -t user_home_t ${TMPDIR}/selinux
 
+sudo setcap 'cap_net_bind_service=+ep' ${TMPDIR}/cap_net_bind_service
 sudo setcap 'cap_chown=+ep' ${TMPDIR}/cap_chown
 sudo setcap 'cap_net_admin=+ep' ${TMPDIR}/cap_net_admin
-sudo setcap 'cap_net_bind_service=+ep' ${TMPDIR}/cap_net_bind_service
 sudo setcap 'cap_net_raw=+ep' ${TMPDIR}/cap_net_raw
 sudo setcap 'cap_sys_ptrace=+ep' ${TMPDIR}/cap_sys_ptrace
 sudo setcap 'cap_chown,cap_net_admin,cap_net_bind_service,cap_net_raw,cap_sys_ptrace=+ep' ${TMPDIR}/cap_all
